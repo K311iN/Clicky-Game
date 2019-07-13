6468be2915ea03b1794eb8657e54cf8e2f6818f0
Kellin <kellinjoshua@gmail.com>
2019-07-12 21:07:01 -0700

    Finish Uploading photos

--- clicky-game/src/App.js
+++ clicky-game/src/App.js
@@ -1,26 +1,80 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Navbar from "./components/Nav";
+import Header from "./components/Header";
+import MemoryCard from "./components/Card";
+import cards from "./cards.json";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const randomizedCards = cards => {
+  for (let i = 0; i < cards.length - 1; i++) {
+    const j = i + Math.floor(Math.random() * (cards.length - i));
+
+    const temp = cards[j];
+    cards[j] = cards[i];
+    cards[i] = temp;
+  }
+  console.log(cards);
+  return cards;
+};
+
+let pickedCards = [];
+
+class App extends React.Component {
+  state = {
+    score: 0,
+    highScore: 0,
+    navbarMessage: "Defeat enemies by clicking on them only once!",
+    cards: cards,
+    randomCards: cards
+  };
+
+  randomCards = randomizedCards(cards);
+
+  pickCard = id => {
+    console.log(id);
+    if (pickedCards.indexOf(id) === -1) {
+      pickedCards.push(id);
+      this.setState({
+        score: this.state.score + 1,
+        navbarMessage: "You guessed correctly!"
+      });
+      if (this.state.score >= this.state.highScore) {
+        this.setState({ highScore: this.state.score + 1 });
+      }
+      this.setState({
+        randomCards: randomizedCards(cards)
+      });
+    } else {
+      this.setState({
+        score: 0,
+        randomCards: randomizedCards(cards),
+        navbarMessage: "You guessed incorrectly!"
+      });
+      pickedCards = [];
+    }
+    console.log(pickedCards);
+  };
+
+  render() {
+    return (
+      <div>
+        <Navbar
+          navbarMessage={this.state.navbarMessage}
+          score={this.state.score}
+          highScore={this.state.highScore}
+        />
+        <Header />
+        {this.state.cards.map(card => (
+          <MemoryCard
+            image={card.image}
+            id={card.id}
+            key={card.id}
+            name={card.id}
+            pickCard={this.pickCard}
+          />
+        ))}
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ clicky-game/src/components/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Header.css";
+
+const Header = () => (
+  <header className="header">
+    <h1 className="title">Clicky Game</h1>
+    <h3>
+      Click on any player to earn points, but don't click the same character twice!
+    
+      GOOD LUCK!
+    </h3>
+  </header>
+);
+export default Header;
